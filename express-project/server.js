950b85462259c5880e25a4bad85f57dbf47b5d1c
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2022-08-30 17:14:07 +0700

    ExpressJS, middleware (custom, json dan router) dan MVC di express (bagian I)

---
+++ express-project/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const friendsRouter = require('./routes/friends.router')
+const messagesRouter = require('./routes/messages.router')
+
+const app = express()
+
+const PORT = 3000
+
+app.use((req, res, next) => {
+    const start = Date.now()
+    next()
+    // actions after respond being send back by endpoint goes here...
+    const delta = Date.now() - start
+    console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`)
+})
+
+app.use(express.json())
+
+app.use('/friends', friendsRouter)
+app.use('/messages', messagesRouter)
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
