7676d5db1c7d111addf47456d2ca037f7414c4d9
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2022-08-31 10:21:51 +0700

    ExpressJS, middleware (custom, json, router dan handlebars) dan MVC di express (bagian II - akhir)

--- express-project/controllers/messages.controller.js
+++ express-project/controllers/messages.controller.js
@@ -1,12 +1,19 @@
+const path = require("path")
+
 function getMessages(req, res) {
-    res.send('<ul><li>Helloo Albert!</li></ul>')
+    res.render("messages", {
+        title: "Messages to my Friends!",
+        friend: "Elon Musk",
+    })
+    // res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
+    // res.send('<ul><li>Helloo Albert!</li></ul>')
 }
 
 function postMessage(req, res) {
-    console.log('Updating messages...')
+    console.log("Updating messages...")
 }
 
 module.exports = {
     getMessages,
-    postMessage
+    postMessage,
 }
