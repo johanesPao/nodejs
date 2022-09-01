ed0e515187d6c844a014820b426b9812c709ddb5
Johanes Indra Pradana Pao <johanes.pao@gmail.com>
2022-09-01 16:12:34 +0700

    Cluster dan Worker menggunakan modul (P)rocess (M)anager 2 NPM

---
+++ performance-example/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const app = express();
+
+function delay(duration) {
+    const startTime = Date.now();
+    while (Date.now() - startTime < duration) {
+        //event loop is blocked...
+    }
+}
+
+app.get("/", (req, res) => {
+    // JSON.stringify({}) => "{}"
+    // JSON.parse("{}") => {}
+    // [5,1,2,3,4].sort()
+    res.send(`Performance example: ${process.pid}`);
+});
+
+app.get("/timer", (req, res) => {
+    delay(4000);
+    res.send(`Beep beep beep! ${process.pid}`);
+});
+
+console.log("Running server.js...");
+
+console.log("Worker process started...");
+app.listen(3000);
